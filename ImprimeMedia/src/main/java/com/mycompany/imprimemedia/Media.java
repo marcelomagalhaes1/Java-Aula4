b41558c234fe780948418d9be6be35c439061bd0
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2023-09-05 18:34:05 -0300

    Add files via upload

---
+++ ImprimeMedia/src/main/java/com/mycompany/imprimemedia/Media.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.imprimemedia;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Media {
+    
+    String nome[] = new String[5];
+    float pr1[] = new float[5];
+    float pr2[] = new float[5];
+    float media[] = new float[5];
+}
